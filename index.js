e5628ce0fa0a37130825d10c1f924bb3164452bd
denisemawilson <denise@denisemawilson>
2026-04-20 02:12:05 -0400

    Updated Site

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach(entry => { 
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.values__container, .quality__container, .steps__container, .testimony__container').forEach(el => observer.observe(el));
+});
